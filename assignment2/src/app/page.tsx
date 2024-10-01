e840bf4cb4fa3dab990d50aa6368abae11b18b46
Maryam Khan <crystal1468592@gmail.com>
2024-10-01 17:25:47 +0500

    making a nextjs app

---
+++ assignment2/src/app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div className="body">
+      {/* Below Code Is With CSS */}
+      <div className="parentDiv">
+        <div className="child1">
+          Hey there,
+          <br />
+          My Name is <span className="name">Maryam</span>
+          <br />I am a Next JS Developer
+        </div>
+        <div className="child2"> <img className="profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcbqG3NZuUc1H7Ks2mXRNuP7Ka37XSrM3Etg&s" /> </div>
+      </div>
+    </div>
+    
+  )};
